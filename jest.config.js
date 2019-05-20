aa441e85f7130f7747504a6f3475d84d4b525460
mattisonZhao <mattison@yeah.net>
2019-05-20 14:06:55 +0800

    详细的测试报告

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,15 @@
 module.exports = {
     verbose: true,
     clearMocks: false,
-    collectCoverage: false,
-    reporters: ["default"],
+
+
+    collectCoverage: true,
+    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**","!**/*.d.ts"],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+    reporters: ["default","jest-junit"],
+
+
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules'],
     globals: {
