a458124eaaa6704e8006b249b5c875dcf1a73440
mattison <mattison@yeah.net>
2019-06-09 14:24:00 +0800

    使用了react portal

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -6,22 +6,54 @@ export default function () {
     const [x, setX] = useState(false);
     return (
         <div>
-            <button onClick={() => {
-                setX(!x)
-            }}>click
-            </button>
-            <Dialog visible={x} buttons={
-                [
-                    <button onClick={()=>{setX(false)}}> cancle </button>,
-                    <button onClick={()=>{}}> ok </button>
-                ]
-            } onClose={()=>{setX(false);}}
-                closeOnClickMask={true}
-            >
-                <div>
-                   <strong>HI</strong>
-                </div>
-            </Dialog>
+            <div>
+                <h1>example 1</h1>
+                <button onClick={() => {
+                    setX(!x)
+                }}>click
+                </button>
+                <Dialog visible={x} buttons={
+                    [
+                        <button onClick={() => {
+                            setX(false)
+                        }}> cancle </button>,
+                        <button onClick={() => {
+                        }}> ok </button>
+                    ]
+                } onClose={() => {
+                    setX(false);
+                }}
+                        closeOnClickMask={true}
+                >
+                    <div>
+                        <strong>HI</strong>
+                    </div>
+                </Dialog>
+            </div>
+            <div>
+                <h1>example 2</h1>
+                <button onClick={() => {
+                    setX(!x)
+                }}>click
+                </button>
+                <Dialog visible={x} buttons={
+                    [
+                        <button onClick={() => {
+                            setX(false)
+                        }}> cancle </button>,
+                        <button onClick={() => {
+                        }}> ok </button>
+                    ]
+                } onClose={() => {
+                    setX(false);
+                }}
+                        closeOnClickMask={true}
+                >
+                    <div>
+                        <strong>HI</strong>
+                    </div>
+                </Dialog>
+            </div>
         </div>
     )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -2,50 +2,50 @@ import * as React from 'react';
 import './dialog.scss';
 import '../index.tsx'
 import {Fragment, ReactElement} from 'react';
+import * as ReactDom from 'react-dom'
 import {scopedClassMaker} from "../classes";
 
 
 interface Props {
     visible: boolean;
-    buttons:Array<ReactElement>;
-    onClose:React.MouseEventHandler;
+    buttons: Array<ReactElement>;
+    onClose: React.MouseEventHandler;
     closeOnClickMask?: boolean;
 }
 
 const scopedClass = scopedClassMaker('roue-dialog');
 const sc = scopedClass;
 const Dialog: React.FunctionComponent<Props> = (props) => {
-    const onClickClose:React.MouseEventHandler = (e)=>{
+    const onClickClose: React.MouseEventHandler = (e) => {
         props.onClose(e)
     };
-    const closeMask:React.MouseEventHandler = (e)=>{
-        if(props.closeOnClickMask){
+    const closeMask: React.MouseEventHandler = (e) => {
+        if (props.closeOnClickMask) {
             onClickClose(e)
         }
-    }
-    return (
-        props.visible ?
-            <Fragment>
-                <div className={sc('mask')} onClick={closeMask} >
-                </div>
-                <div className={sc()}>
-                    <header className={sc('header')}>
-                        提 醒！
-                    </header>
-                    <main className={sc('main')}>
-                        {props.children}
-                    </main>
-                    <footer className={sc('footer')}>
-                        {props.buttons.map((button,index)=>
-                            React.cloneElement(button,{key:index})
-                        )}
-                    </footer>
-                </div>
-            </Fragment> :
-            null
-    )
+    };
+    const x = props.visible ?
+        <Fragment>
+            <div className={sc('mask')} onClick={closeMask}>
+            </div>
+            <div className={sc()}>
+                <header className={sc('header')}>
+                    提 醒！
+                </header>
+                <main className={sc('main')}>
+                    {props.children}
+                </main>
+                <footer className={sc('footer')}>
+                    {props.buttons.map((button, index) =>
+                        React.cloneElement(button, {key: index})
+                    )}
+                </footer>
+            </div>
+        </Fragment> :
+        null;
+    return ReactDom.createPortal(x, document.body)
 };
 Dialog.defaultProps = {
-    closeOnClickMask : false
-}
-export default Dialog
+    closeOnClickMask: false
+};
+export default Dialog 
