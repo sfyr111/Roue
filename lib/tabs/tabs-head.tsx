a78c998fb7777b0892943a243fafdcfd0aedd7e8
mattisonZhao <mattison@yeah.net>
2019-08-07 08:35:48 +0800

    添加tabs 线效果

--- lib/tabs/tabs-head.tsx
+++ lib/tabs/tabs-head.tsx
@@ -1,24 +1,59 @@
 import * as React from "react";
 import {scopedClassMaker} from "../helper/classes";
-import {HTMLAttributes} from "react"
+import {HTMLAttributes, useContext, useEffect, useRef} from "react"
+import {MyContext} from "./reducer";
 
-interface Props extends HTMLAttributes<HTMLElement>{
+interface Props extends HTMLAttributes<HTMLElement> {
 }
 
 
-const sc=scopedClassMaker("roue-tabs-head");
+const sc = scopedClassMaker("roue-tabs-head");
 
 
-const TabsHead:React.FunctionComponent<Props> = (props)=>{
-    const {className,...rest} = props;
+const TabsHead: React.FunctionComponent<Props> = (props) => {
+        const {className, ...rest} = props;
 
 
-    return <div className={sc({"":true},{extra:className})}
-                {...rest}
-    >
-        {props.children}
-    </div>
-};
+        const headLine = useRef(document.createElement("div"));
+        const tabsHead = useRef(document.createElement("div"));
+        const getHeadLine = () => {
+            return headLine.current
+        };
+        const getTabsHead = () => {
+            return tabsHead.current
+        };
+
+
+        const {state} = useContext(MyContext);
+        const getLinePosition = () => {
+            const parent = getTabsHead().getClientRects();
+            const el = document.querySelector(".roue-tabs-items-active") || document.createElement("div");
+            const {width, left} = el.getBoundingClientRect();
+            const lineLeft = left - parent[0].left;
+            getHeadLine().style.width = `${width}px`;
+            getHeadLine().style.left = `${lineLeft}px`;
+        };
+
+
+
+        useEffect(() => {
+            setTimeout(() => {
+                getLinePosition()
+            }, 0)
+        }, state)
+
+
+
+
+        return <div ref={tabsHead} className={sc({"": true}, {extra: className})}
+                    {...rest}
+        >
+            {props.children}
+            <div ref={headLine} className="roue-tabs-head-line"/>
+        </div>
+
+    }
+;
 
 
 export default TabsHead
