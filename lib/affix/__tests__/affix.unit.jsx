ea55ac52bd5dae33e1720849f62cddf0ac8e379c
mattisonZhao <mattison@yeah.net>
2019-08-21 16:06:18 +0800

    test

---
+++ lib/affix/__tests__/affix.unit.jsx
@@ -0,0 +1,23 @@
+import * as renderer from 'react-test-renderer';
+import * as React from 'react';
+import {mount} from 'enzyme';
+import Affix from '../affix';
+
+describe("Affix", () => {
+    it("affix exist", () => {
+        const json = renderer.create(<Affix/>).toJSON();
+        expect(json).toMatchSnapshot();
+    });
+    it("affix content", () => {
+        const component = mount(<Affix>12345</Affix>);
+
+        expect(component.find(".roue-affix").text()).toEqual("12345");
+    });
+  /*  it("affix fixed", () => {
+        const component = mount(<Affix>12345</Affix>);
+        Object.defineProperty(window, 'scrollY', {value: 9990, writable: true})
+        console.log(component.find(".roue-affix").props())
+        component.find(".roue-affix").prop('onScrollY')
+        expect(component.find(".roue-affix").prop('style')).toHaveProperty('position', 'fixed')
+    });*/
+});
--- lib/affix/affix.tsx
+++ lib/affix/affix.tsx
@@ -10,45 +10,45 @@ interface Props extends HTMLAttributes<HTMLElement> {
 
 
 const Affix: React.FunctionComponent<Props> = (props) => {
-        const {
-            className,
-            ...rest
-        } = props;
-        const affix = useRef(null);
-        // not good functional
-        const addStyle = (offset: number, affix: any) => {
-            return () => {
-                const height = window.scrollY;
-                if (offset === height || height > offset) {
-                    affix.current.style.position = "fixed";
-                    affix.current.style.top = 0
-                } else {
-                    affix.current.style.position = "static";
-                    affix.current.style.top = "unset"
-                }
+    const {
+        className,
+        ...rest
+    } = props;
+    const affix = useRef(null);
+    // not good functional
+    const addStyle = (offset: number, affix: any) => {
+        return () => {
+            const height = window.scrollY;
+            if (offset === height || height > offset) {
+                affix.current.style.position = "fixed";
+                affix.current.style.top = 0;
+            } else {
+                affix.current.style.position = "static";
+                affix.current.style.top = "unset";
             }
         };
-        const fixedElement = (affix: any) => {
-            const offset =  affix.current.offsetTop;
-            return  addStyle(offset, affix)
-        };
-
-        useEffect(() => {
-            const onscrollHandle =  fixedElement(affix);
-            window.addEventListener("scroll", onscrollHandle);
-            return ()=>{
-                window.removeEventListener("scroll", onscrollHandle)
-            }
-        });
-        return (
-            <div ref={affix}
-                 className={
-                     sc(
-                         {
-                             "": true,
-                         }, {extra: className})}{...rest} >
-                {props.children}
-            </div>
-        )
     };
-export default Affix
+    const fixedElement = (affix: any) => {
+        const offset = affix.current.offsetTop;
+        return addStyle(offset, affix);
+    };
+
+    useEffect(() => {
+        const onscrollHandle = fixedElement(affix);
+        window.addEventListener("scroll", onscrollHandle);
+        return () => {
+            window.removeEventListener("scroll", onscrollHandle);
+        };
+    });
+    return (
+        <div ref={affix}
+             className={
+                 sc(
+                     {
+                         "": true
+                     }, {extra: className})}{...rest} >
+            {props.children}
+        </div>
+    );
+};
+export default Affix;
